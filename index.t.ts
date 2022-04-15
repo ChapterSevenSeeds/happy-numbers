9fd31f51e69a5cb649c9edd862f21a0afc330425
Tyson Jones <ballisticmissle@gmail.com>
2022-04-15 11:10:49 -0600

    Final push

--- index.t.ts
+++ index.t.ts
@@ -1,4 +1,18 @@
 declare module "happy-numbers2" {
+    /**
+     * Generates happy numbers.
+     * @param {number} count The amount of happy numbers to generate.
+     * @param {number} min The inclusive minimum number.
+     * @param {number} max The inclusive maximum number.
+     * @returns An array of happy numbers.
+     */
     export function generateHappyNumbers(count: number, min: number, max: number): number[];
+
+
+    /**
+     * Determines if the given number is happy.
+     * @param {number} num The input number.
+     * @returns True if the number is happy. False otherwise.
+     */
     export function isHappy(num: number): boolean;
 }
